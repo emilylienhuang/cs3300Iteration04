cd7bf5a4fc8fa562b869222a2870bf5b0e775d99
emilylienhuang <eng@uccs.edu>
2022-11-07 23:01:36 +0000

    correct project description

--- db/migrate/20221107214057_create_projects.rb
+++ db/migrate/20221107214057_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
       t.string :title
-      t.text :descrition
+      t.text :description
 
       t.timestamps
     end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Project, type: :model do
     it "should be able to save project" do
       project = Project.new(title: "Title", description: "Some description content goes here")
       expect(project.save).to eq(true)
-4
+
     end
   end
   context "scopes tests" do
