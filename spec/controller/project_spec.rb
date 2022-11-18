1e7884df88cff85a042a0826e7c95a06d02f6fd1
emilylienhuang <eng@uccs.edu>
2022-11-18 22:14:53 +0000

    adding comments

--- spec/controller/project_spec.rb
+++ spec/controller/project_spec.rb
@@ -1,17 +1,21 @@
-require 'rails_helper'
+require 'rails_helper' #use rails' built in helper methods
 
-RSpec.describe ProjectsController, type: :controller do
-    context "GET #index" do
+RSpec.describe ProjectsController, type: :controller do #test fot the project controller; it should do the following
+    context "GET #index" do #when calling the index method with the get command ...
         it "returns a success response" do
             get :index
+            # the response of this call should be successful. i.e projects variable in the project controller should equal all the projects in the database
             expect(response).to be_successful
         end
     end
 
+    #the above test checked if the project controller is able to access the database
+
     context "Get #show" do
-        let!(:project) { Project.create(title: "Test title", description: "Test Description") }
+    #when calling the method show from a get request...
+        let!(:project) { Project.create(title: "Test title", description: "Test Description") } #create a dummy project to run a test on
         it "returns a success response" do
-            get :show, params: { id: project }
+            get :show, params: { id: project } #in response to show call, there should be a successful response displaying the project
             expect(response).to be_successful
         end
     end
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,37 +1,50 @@
-require 'rails_helper'
+require 'rails_helper' #using rails helper methods
 
 RSpec.feature "Projects", type: :feature do
   context "Create new project" do
+    #when you click to create a new project (this will be executed before all the tests)
+    #so there will be a dummy project used for all the tests
     before (:each) do
+      #each time you do so you visit a new project path 
       visit new_project_path
       within("form") do
+        #there will be a form there, and it will fill in title with test title
         fill_in "Title", with: "Test title"
       end
-    end
+    end #that will conclude how we define our dummy project
   
     scenario "should be successful" do
-      fill_in "Description", with: "Test description"
+      fill_in "Description", with: "Test description" #if in our dummy project description field is able to be filled in with a test description...
       click_button "Create Project"
+      #when you click the button to create a project, it is expected that the page will display
+      #project was successfully created
       expect(page).to have_content("Project was successfully created")
     end
   
     scenario "should fail" do
       click_button "Create Project"
+      #if the user clicks create project, and the description is blank
+      #then the page should display a message that the description can't be blank
       expect(page).to have_content("Description can't be blank")
     end
   end
 
-  context "Update project" do
-    let(:project) { Project.create(title: "Test title", description: "Test content") }
+  context "Update project" do 
+    #running tests on updating project
+    let(:project) { Project.create(title: "Test title", description: "Test content") } #create a dummy project and let this refer to project
     before(:each) do
-      visit edit_project_path(project)
+      #for each run of this test, 
+      visit edit_project_path(project) #visit the project path to edit the project, will be nil if unable to do so
     end
 
     scenario "should be successful" do
       within("form") do
         fill_in "Description", with: "New description content"
+        #project creation should be successful given that 
+        #in the project creation form the description field is able to be filled in with new description content
       end
       click_button "Update Project"
+      #when you click the button to update the project, the web page should display a message the the project was updated successfully
       expect(page).to have_content("Project was successfully updated")
     end
 
@@ -43,6 +56,8 @@ RSpec.feature "Projects", type: :feature do
       expect(page).to have_content("Description can't be blank")
     end
   end
+  #above, project creation would fail  if the descrition field was entered in blank on the form
+  #when you click the button to update the project the error Description can't be blank should be displayed on the screen
 
 # Add the following code but comment it out. We are working on getting this to work
 #  context "Remove existing project" do
